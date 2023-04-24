33b6a8a32d9918b85bda7855fde29eb7e9f9f80c
Chinweike Michael Chinonso <infinity_michael@yahoo.com>
2023-04-24 23:41:41 +0100

    feat(project setup):Implemented project setup The tools were used for this setup eslint,prettier, husky, lint-staged

--- App.tsx
+++ App.tsx
@@ -1,14 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const App: React.FunctionComponent = () => {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style='auto' />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -18,3 +19,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,22 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      'inline-dotenv',
+      [
+        'module-resolver',
+        {
+          alias: {
+            components: './components',
+            libs: './libs',
+            assets: './assets',
+            screens: './screens',
+            navigations: './navigations',
+          },
+          extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        },
+      ],
+    ],
   };
 };
