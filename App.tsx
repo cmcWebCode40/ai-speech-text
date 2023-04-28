d959d44fa8b3f642ae26deb4fd72262944f41367
Chinweike Michael Chinonso <infinity_michael@yahoo.com>
2023-04-28 06:39:24 +0100

    feat: Added dependecies for speech to text logic Updated env sample file

--- App.tsx
+++ App.tsx
@@ -1,22 +1,19 @@
+//url polyfill
+import 'react-native-url-polyfill/auto';
+
 import { StatusBar } from 'expo-status-bar';
 import { AppContext } from 'libs/context';
-import { useLoadFonts } from 'libs/hooks';
 import React from 'react';
+import Toast from 'react-native-toast-message';
 
-import { Typography } from 'components/atoms';
 import { MainNavigation } from 'components/navigations';
 
 const App: React.FunctionComponent = () => {
-  const { isFontLoaded } = useLoadFonts();
-
-  if (!isFontLoaded) {
-    return <Typography>Loading fonts</Typography>;
-  }
-
   return (
     <AppContext>
       <StatusBar style='auto' />
       <MainNavigation />
+      <Toast />
     </AppContext>
   );
 };
